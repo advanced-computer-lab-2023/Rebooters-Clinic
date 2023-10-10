0cdd5f640a471c58db6cbd00dcc76ad0f81560b0
BasselTharwat <basseltharwat01@gmail.com>
2023-10-11 00:03:33 +0300

    pushing for safety

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar= () =>{
         <header>
             <div className="container">
                 <Link to= "/">
-                   <h1>Workout Buddy</h1> 
+                   <h1>el7a2ni</h1> 
                 </Link>
 
             </div>
