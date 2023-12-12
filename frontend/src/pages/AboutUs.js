e992efbcb9116d2f0b7e1d25aab6c2c9a1241153
Maya <mayamokhtar2002@gmail.com>
2023-12-13 01:12:06 +0200

    fixed aboutUs+contactPage

--- frontend/src/pages/AboutUs.js
+++ frontend/src/pages/AboutUs.js
@@ -1,12 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStethoscope, faUserMd, faHandHoldingHeart, faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import loginCoverImage from '../styles/carousel-2.jpg';
 import bgImage from '../styles/carousel-1.jpg';
 import heroImage from '../styles/doctor-background.jpg';
 import "../styles/au.css";
+import { CSSTransition } from 'react-transition-group';
+
+
 
 const AboutUs = () => {
+  const [showMissionCard, setShowMissionCard] = useState(false);
+  const [showTrustedCompanyCard, setShowTrustedCompanyCard] = useState(false);
+  const [showContent, setShowContent] = useState(false);
+
+  const missionCardRef = useRef(null);
+  const trustedCompanyCardRef = useRef(null);
+
+  const handleIntersection = (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        switch (entry.target) {
+          case missionCardRef.current:
+            setShowMissionCard(true);
+            break;
+          case trustedCompanyCardRef.current:
+            setShowTrustedCompanyCard(true);
+            break;
+          default:
+            break;
+        }
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5,
+    };
+    const timeout = setTimeout(() => {
+      setShowContent(true);
+    }, 500);
+
+
+    const observer = new IntersectionObserver(handleIntersection, options);
+
+    
+    if (missionCardRef.current) {
+      observer.observe(missionCardRef.current);
+    }
+    if (trustedCompanyCardRef.current) {
+      observer.observe(trustedCompanyCardRef.current);
+    }
+
+    return () => {
+      clearTimeout(timeout);
+      if (missionCardRef.current) {
+        observer.unobserve(missionCardRef.current);
+      }
+      if (trustedCompanyCardRef.current) {
+        observer.unobserve(trustedCompanyCardRef.current);
+      }
+
+    };
+  }, []);
+
   return (
     <div className="site-wrap">
 <div className="login-cover">
@@ -77,19 +138,21 @@ const AboutUs = () => {
             </a>
           </div>
         </nav>
-        <div className="text-center" style={{ marginLeft: '40px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white', zIndex: 2 }}>
-            <h1>About Us</h1>
-            <p>
-              Welcome to Our Clinic, your trusted partner in healthcare. At Our Clinic, we are committed to providing high-quality medical services to our valued patients.
-            </p>
-          </div>
-          <div className='xy-cover' style={{ backgroundImage: `url(${loginCoverImage})`, backgroundSize: 'cover', height: '100vh', position: 'relative', zIndex: 1 }}>
-           
-      </div>
-
-      <div className="container-fluid mt-5" style={{ marginLeft: '20px' }}> {/* Adjust the margin-left as needed */}
-        {/* Mission Card */}
-        <div className="row justify-content-center mb-5">
+        
+        <div className="text-center" style={{ marginLeft: '30px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white', zIndex: 2 }}>
+          <CSSTransition in={showContent} timeout={500} classNames="fade" unmountOnExit>
+            <div>
+              <h1 style={{ fontSize: '52px' }}>About Us</h1>
+              <p style={{ fontSize: '28px' }}>
+                Welcome to Our Clinic, your trusted partner in healthcare. At Our Clinic, we are committed to providing high-quality medical services to our valued patients.
+              </p>
+            </div>
+          </CSSTransition>
+        </div>
+        <div className='xy-cover' style={{ backgroundImage: `url(${loginCoverImage})`, backgroundSize: 'cover', height: '100vh', position: 'relative', zIndex: 0 }}></div>
+        <div className="container-fluid mt-5" style={{ marginLeft: '20px' }}>
+          {/* Mission Card */}
+        <div className={`row justify-content-center mb-5 ${showMissionCard ? 'fade-in slide-from-left' : ''}`} ref={missionCardRef}>
           <div className="col-md-8">
             <div className="site-section bg-light custom-border-bottom" data-aos="fade">
               <div className="row mb-5">
@@ -113,13 +176,14 @@ const AboutUs = () => {
                     </p>
                   </div>
                 </div>
+                
               </div>
             </div>
           </div>
         </div>
 
         {/* Trusted Company Card */}
-        <div className="row justify-content-center mb-5">
+        <div className={`row justify-content-center mb-5 ${showTrustedCompanyCard ? 'fade-in slide-from-right' : ''}`} ref={trustedCompanyCardRef}>
           <div className="col-md-8">
             <div className="site-section bg-light custom-border-bottom" data-aos="fade">
               <div className="row mb-5">
@@ -146,7 +210,7 @@ const AboutUs = () => {
         </div>
 
         {/* Free Services, Patient-Centered Care, Expert Healthcare Support */}
-        <div className="row justify-content-center mb-5">
+        <div className="row justify-content-center mb-5 slide-in">
           <div className="col-lg-4 mb-4 mb-lg-0">
             <div className="text">
               <h2><FontAwesomeIcon icon={faStethoscope} /> Comprehensive Services</h2>
--- frontend/src/pages/ContactPage.js
+++ frontend/src/pages/ContactPage.js
@@ -1,100 +1,119 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarkerAlt,faPhone,faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import loginCoverImage from '../styles/carousel-2.jpg';
+import Navbar from '../components/Navbar';
+import "../styles/au.css";
 
 const Conc = () => {
-    return (
-      <div className="site-wrap">
-<div className="login-cover">
-      <div className="cover-color">
-        <nav class="navbar navbar-expand-lg bg-white navbar-light shadow-sm px-5 py-3 py-lg-0">
-        <img src={"clinic-logo.png"} width="100"/>
-          <a href="/" class="navbar-brand p-0">
-            <h1 class="m-0 text-primary">
-              <i class="fa fa-tooth me-2"></i>El7a2ni
-            </h1>
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarCollapse"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarCollapse">
-            <div class="navbar-nav ms-auto py-0">
-              <a href="/index" class="nav-item nav-link active">
-                Home
-              </a>
-              <a href="/about" class="nav-item nav-link">
-                About
-              </a>
-              <a href="/service" class="nav-item nav-link">
-                Service
-              </a>
-              <div class="nav-item dropdown">
-                <a
-                  href="#"
-                  class="nav-link dropdown-toggle"
-                  data-bs-toggle="dropdown"
-                >
-                  Pages
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    // Set a delay to trigger the animation after a short period (e.g., 500 milliseconds)
+    const timeout = setTimeout(() => {
+      setShowContent(true);
+    }, 500);
+
+    // Clear the timeout on component unmount
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div className="site-wrap">
+      <div className="login-cover">
+        <div className="cover-color">
+          <nav className="navbar navbar-expand-lg bg-white navbar-light shadow-sm px-5 py-3 py-lg-0">
+            <img src={"clinic-logo.png"} width="100" alt="Clinic Logo" />
+            <a href="/" className="navbar-brand p-0">
+              <h1 className="m-0 text-primary">
+                <i className="fa fa-tooth me-2"></i>El7a2ni
+              </h1>
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarCollapse"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <div className="navbar-nav ms-auto py-0">
+                <a href="/index" className="nav-item nav-link active">
+                  Home
                 </a>
-                <div class="dropdown-menu m-0">
-                  <a href="price.html" class="dropdown-item">
-                    Pricing Plan
+                <a href="/about" className="nav-item nav-link">
+                  About
+                </a>
+                <a href="/service" className="nav-item nav-link">
+                  Service
+                </a>
+                <div className="nav-item dropdown">
+                  <a
+                    href="#"
+                    className="nav-link dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                  >
+                    Pages
                   </a>
-                  <a href="team.html" class="dropdown-item">
-                    Our Dentist
-                  </a>
-                  <a href="testimonial.html" class="dropdown-item">
-                    Testimonial
-                  </a>
-                  <a href="appointment.html" class="dropdown-item">
-                    Appointment
-                  </a>
+                  <div className="dropdown-menu m-0">
+                    <a href="price.html" className="dropdown-item">
+                      Pricing Plan
+                    </a>
+                    <a href="team.html" className="dropdown-item">
+                      Our Dentist
+                    </a>
+                    <a href="testimonial.html" className="dropdown-item">
+                      Testimonial
+                    </a>
+                    <a href="appointment.html" className="dropdown-item">
+                      Appointment
+                    </a>
+                  </div>
                 </div>
+                <a href="/contact" className="nav-item nav-link">
+                  Contact
+                </a>
               </div>
-              <a href="/contact" class="nav-item nav-link">
-                Contact
+              <button
+                type="button"
+                className="btn text-dark"
+                data-bs-toggle="modal"
+                data-bs-target="#searchModal"
+              >
+                <i className="fa fa-search"></i>
+              </button>
+              <a href="" className="btn btn-primary py-2 px-4 ms-3">
+                Appointment
               </a>
             </div>
-            <button
-              type="button"
-              class="btn text-dark"
-              data-bs-toggle="modal"
-              data-bs-target="#searchModal"
-            >
-              <i class="fa fa-search"></i>
-            </button>
-            <a href="" class="btn btn-primary py-2 px-4 ms-3">
-              Appointment
-            </a>
-          </div>
-        </nav>        
-        <div className="text-center" style={{ marginLeft: '30px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white', zIndex: 2 }}>
-            <h1>Contact Info</h1>
-            <ul className="list-unstyled">
-              <li className="address">
-                <FontAwesomeIcon icon={faMapMarkerAlt} /> GUC - El Tagamoa El Khames New Cairo City - Egypt
-              </li>
-              <li className="phone">
-                <FontAwesomeIcon icon={faPhone} /> <a href="tel://23923929210">+1 23456789</a>
-              </li>
-              <li className="email">
-                <FontAwesomeIcon icon={faEnvelope} /> rebootersteam9@gmail.com
-              </li>
-            </ul>
+          </nav>
+          <div className="text-center" style={{ marginLeft: '30px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', color: 'white', zIndex: 2 }}>
+            <CSSTransition in={showContent} timeout={500} classNames="fade" unmountOnExit>
+              <div>
+                <h1 style={{ fontSize: '52px'}}>Contact Info</h1>
+                <ul className="list-unstyled">
+                  <li className="address">
+                    <FontAwesomeIcon icon={faMapMarkerAlt} /> GUC - El Tagamoa El Khames New Cairo City - Egypt
+                  </li>
+                  <li className="phone">
+                    <FontAwesomeIcon icon={faPhone} /> <a href="tel://23923929210">+1 23456789</a>
+                  </li>
+                  <li className="email">
+                    <FontAwesomeIcon icon={faEnvelope} /> rebootersteam9@gmail.com
+                  </li>
+                </ul>
+              </div>
+            </CSSTransition>
           </div>
           <div className='xy-cover' style={{ backgroundImage: `url(${loginCoverImage})`, backgroundSize: 'cover', height: '100vh', position: 'relative', zIndex: 1 }}>
-            
-                </div>
-                </div>
-                </div>
-                </div>
-    );
+            {/* Your Background Image */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
-export default Conc;
 
+export default Conc;
